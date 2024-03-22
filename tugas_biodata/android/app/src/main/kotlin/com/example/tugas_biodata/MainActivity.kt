26b0713b82065ae88f2cd62190d57e369de94654
rizalarfani <arfanirizal22@gmail.com>
2024-03-22 08:21:07 +0700

    firsh commit

---
+++ tugas_biodata/android/app/src/main/kotlin/com/example/tugas_biodata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas_biodata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tugas_biodata/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tugas_biodata/screen/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Biodata',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: false,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
