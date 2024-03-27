788a88e332b93c7917077cc796c4944852c26a06
rizalarfani <arfanirizal22@gmail.com>
2024-03-27 10:34:19 +0700

    tugas 2

---
+++ tugas_dashboard/lib/dashboard.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              height: 150,
+              decoration: BoxDecoration(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
