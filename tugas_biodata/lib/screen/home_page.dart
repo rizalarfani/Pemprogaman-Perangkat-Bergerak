a16c16cd2fdea895f51b987636f15820d1be1f17
rizalarfani <arfanirizal22@gmail.com>
2024-03-28 09:41:13 +0700

    remove tugas dashboard

--- tugas_biodata/lib/screen/home_page.dart
+++ tugas_biodata/lib/screen/home_page.dart
@@ -8,111 +8,206 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              'Biodata Mahasiswa',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 15),
-            Container(
-              margin: const EdgeInsets.all(10),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color.fromRGBO(16, 96, 175, 1),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(10),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                'Biodata Mahasiswa',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.15),
-                    offset: const Offset(0, 2),
-                    blurRadius: 7,
-                    spreadRadius: 0,
-                  ),
-                ],
               ),
-              child: const Column(
-                children: [
-                  SizedBox(height: 10),
-                  CircleAvatar(
-                    radius: 70,
-                    backgroundColor: Colors.white,
-                    child: Padding(
-                      padding: EdgeInsets.all(3),
-                      child: CircleAvatar(
-                        radius: 70,
-                        backgroundImage:
-                            AssetImage('assets/images/profile.JPG'),
+              const SizedBox(height: 15),
+              Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(16, 96, 175, 1),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      offset: const Offset(0, 2),
+                      blurRadius: 7,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: const Column(
+                  children: [
+                    SizedBox(height: 10),
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: CircleAvatar(
+                          radius: 70,
+                          backgroundImage:
+                              AssetImage('assets/images/profile.JPG'),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    'Mohamad Rijal Arfani',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 23,
-                      fontWeight: FontWeight.w700,
+                    SizedBox(height: 5),
+                    Text(
+                      'Mohamad Rijal Arfani',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'NIM',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'NIM',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'A18.2023.00046',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
+                        Text(
+                          'A18.2023.00046',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Fakultas',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Fakultas',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      Text(
-                        'Ilmu Komputer',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                        Text(
+                          'Ilmu Komputer',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 15),
+              Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(16, 96, 175, 1),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      offset: const Offset(0, 2),
+                      blurRadius: 7,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: const Column(
+                  children: [
+                    SizedBox(height: 10),
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: CircleAvatar(
+                          radius: 70,
+                          backgroundImage:
+                              AssetImage('assets/images/aziz.jpeg'),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Mohamad Abdul Aziz',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'NIM',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Text(
+                          'A18.2023.00043',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Fakultas',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Text(
+                          'Ilmu Komputer',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- tugas_dashboard/android/app/src/main/kotlin/com/example/tugas_dashboard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tugas_dashboard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
